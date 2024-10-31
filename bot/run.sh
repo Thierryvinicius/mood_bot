f654fb89a4a8afcbc50e1ec2cbe066ccf46d9747
Lebeno <111296000+lescano-leo@users.noreply.github.com>
2024-10-30 23:09:25 -0300

    Update run.sh

--- bot/run.sh
+++ bot/run.sh
@@ -1,7 +1,2 @@
-# Script para economizar digitação :-)
-#
-# Coloque seu token em meu_token.txt mas CUIDADO
-# para não subir o arquivo com sua chave no GIT e
-# deixar ela pública (alguém pode capturar o seu BOT !!!)
-
+# Executa o script coletaImagens.py usando o token armazenado em meu_token.txt.
 cat meu_token.txt | xargs python coletaImagens.py
